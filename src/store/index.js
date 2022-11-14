184ca0ed158d6f57e56214f5539f12a2a79e1534
Erika Green <erika.green@sureswiftcapital.com>
2022-11-14 08:13:54 -0500

    switched from createStore to configureStore in index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,37 +1,32 @@
+import { ReactReduxContext } from "react-redux";
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
+//slice of global state. you can create different slices to help make it more maintainable
+const counterSlice = createSlice({
+  //ever slice needs a name
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      //redux toolkit detects code, automatically clones, and change the state immutably
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+    //   state.counter = state.counter + action.amount;
+    state.counter += action.amount
+    },
+    toggleCounter(state) {
+        state.showCounter = !state.showCounter
+    },
+  },
+});
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggleCounter") {
-    return { 
-        counter: state.counter, 
-        showCounter: !state.showCounter};
-  }
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = createStore(counterSlice.reducer);
 
 export default store;
